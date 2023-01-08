7b46d926b6d6e23b181e031ea1c4181161c0b4b4
Heidi Ganac Nicholson <heidiganac@gmail.com>
2023-01-08 15:11:40 -0600

    created global array and function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,30 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+// 1. 
+//let basket = [];
+
+//function addItem('item'); {
+ // TODO: add new item to global array 'basket'
+ addItem.push('item');
+//return true;
+} // end addItem function
+
+// 2. 
+    //- Create a function called `listItems`. It should:
+//function listItems(); {
+    //- loop over the items in the `basket` array
+}
+    //- console.log each individual item on a new line
+ //   console.log('new line 1', );
+ //   console.log('new line 2', );
+ //   console.log('new line 3', );
+
+// 3. 
+    // Create a function called 'empty'
+//function empty(); {
+    // reset the `basket` to an empty array
+// for basket = basket.length = 0; basket--);
+}
+// console.log('reset basket to empty', empty());
